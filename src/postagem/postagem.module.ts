f0c2690c59e1f7a574b4293fa273f39dd54cdd75
joyceravie <joyce.rvs@hotmail.com>
2024-04-19 02:02:04 -0300

    adiciona CRUD

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostagemController } from './controllers/PostagemController';
 import { Postagem } from './entities/postagem.entity';
+import { PostagemService } from './services/postagem.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Postagem])],
-  providers: [],
-  controllers: [],
+  providers: [PostagemService],
+  controllers: [PostagemController],
   exports: [TypeOrmModule],
 })
 export class PostagemModule {}
